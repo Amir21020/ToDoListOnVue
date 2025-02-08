c98ea4a7027222c8a059bab2419b795455024224
Amir Amirov <mergalievradmir484@gmail.com>
2025-02-08 13:24:50 +0300

    AddTaskListComponent

---
+++ src/components/ListTask.vue
@@ -0,0 +1,28 @@
+<script setup>
+
+import Task from './Task.vue';
+
+defineProps({
+  removeTaskHandler : Function,
+  openUpdateModal : Function,
+  onUpdateComplete : Function,
+  tasks : Array,
+  isBackgroundColorLight : Boolean,
+})
+
+</script>
+
+<template>
+  <div class="flex flex-col mt-5 items-center justify-start">
+    <Task  v-for="(task, index) in tasks"
+    :key="task.id"
+    :title="task.title"
+    :isComplete="task.isComplete"
+    :index="index"
+    :is-background-color-light="isBackgroundColorLight"
+    @onClickRemoveTask="removeTaskHandler(task)"
+    @onClickOpenUpdateModal="openUpdateModal(task)"
+    @onUpdateComplete="onUpdateComplete(task)"
+    />
+  </div>
+</template>
