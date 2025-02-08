6cc0712a06a50a0bf11e70258a0fe380be7454e1
Amir Amirov <mergalievradmir484@gmail.com>
2025-02-08 13:24:34 +0300

    AddFooterComponent

---
+++ src/components/Footer.vue
@@ -0,0 +1,13 @@
+<script setup>
+
+const emit = defineEmits(['openAddTaskModal'])
+</script>
+
+
+<template>
+  <div class="flex justify-end mt-auto  mr-96  items-end">
+      <button @click="$emit('openAddTaskModal')">
+        <img src="/Add button.svg" alt="addTask">
+      </button>
+    </div>
+</template>
